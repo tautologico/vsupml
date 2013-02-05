14ee8df01e6d755a8faae0d38779f1c416fdf6ab
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-02-04 21:19:41 -0300

    Added utility functions to tokenize files and strings.

--- tokenstream.ml
+++ tokenstream.ml
@@ -44,6 +44,35 @@ let main fname =
       | _ -> print_endline (tok2str tok); main_loop (Lexer.next_token lexbuf) in
   main_loop (Lexer.next_token lexbuf)
 
+(** Fold left over the stream of tokens coming from lexbuf. *)
+let foldl_map_tokens f lexbuf eofval =
+  let rec main_loop tok =
+    match tok with
+      EOF -> eofval
+    | _ -> f tok (main_loop Lexer.next_token lexbuf) in
+  main_loop (Lexer.next_token lexbuf)
+
+let dump_tokens fname =
+  let infile = open_in fname in
+  let lexbuf = Lexing.from_channel infile in
+  foldl_tokens (fun tok _ -> print_endline (tok2str tok)) lexbuf ()
+
+(** Tokenize a lexer buffer until EOF and gather tokens in a list. Used by
+    other tokenizing functions. *)
+let lexbuf_to_list lexbuf =
+  foldl_tokens (fun tok rest -> tok :: rest) lexbuf []
+
+(** Tokenize an input file and gather tokens in a list *)
+let tokenize_file fname =
+  let infile = open_in fname in
+  let lexbuf = Lexing.from_channel infile in
+  lexbuf_to_list lexbuf
+
+let tokenize_string str =
+  let lexbuf = Lexing.from_string str in
+  lexbuf_to_list lexbuf
+
+
 let _ =
   if Array.length Sys.argv < 2 then
     print_endline "Input file required"
